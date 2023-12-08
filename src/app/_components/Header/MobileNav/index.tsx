3c8e10314fa5c9339e19bf92390c08f6ed976d33
dzekiczan1 <p.rzadkowolski@gmail.com>
2023-12-08 19:30:08 +0100

    create new header component

---
+++ src/app/_components/Header/MobileNav/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { Header } from '../../../../payload/payload-types'
+
+const MobileNav = ({ header }: { header: Header }) => {
+  return <div>index</div>
+}
+
+export default MobileNav
