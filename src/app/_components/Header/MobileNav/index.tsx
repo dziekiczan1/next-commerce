b017fd5ec78a4a10a38211f729afd6e3a73ba921
dzekiczan1 <p.rzadkowolski@gmail.com>
2023-12-08 19:57:37 +0100

    customize styles of header

--- src/app/_components/Header/MobileNav/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import { Header } from '../../../../payload/payload-types'
-
-const MobileNav = ({ header }: { header: Header }) => {
-  return <div>index</div>
-}
-
-export default MobileNav
