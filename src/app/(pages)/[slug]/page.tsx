d137f647b7815b7643d5eb827d889bea6c80568e
dzekiczan1 <p.rzadkowolski@gmail.com>
2023-12-13 17:07:38 +0100

    modify display of custom hero

--- src/app/(pages)/[slug]/page.tsx
+++ src/app/(pages)/[slug]/page.tsx
@@ -59,9 +59,10 @@ export default async function Page({ params: { slug = 'home' } }) {
   return (
     <React.Fragment>
       {slug === 'home' ? (
-        <Gutter>
+        <section>
           <Hero {...hero} />
-        </Gutter>
+          <Gutter>{/* categories */}</Gutter>
+        </section>
       ) : (
         <>
           {' '}
