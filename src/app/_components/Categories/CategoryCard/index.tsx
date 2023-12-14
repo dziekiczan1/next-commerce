c95495205af997d467f0af6254b0ed759529c542
dzekiczan1 <p.rzadkowolski@gmail.com>
2023-12-14 17:58:59 +0100

    add filter provider

--- src/app/_components/Categories/CategoryCard/index.tsx
+++ src/app/_components/Categories/CategoryCard/index.tsx
@@ -2,7 +2,8 @@
 import React from 'react'
 import Link from 'next/link'
 
-import { Category } from '../../../../payload/payload-types'
+import { Category, Media } from '../../../../payload/payload-types'
+import { useFilter } from '../../../_providers/Filter'
 
 import classes from './index.module.scss'
 
@@ -12,6 +13,7 @@ type CategoryCardProps = {
 
 const CategoryCard = ({ category }: CategoryCardProps) => {
   const media = category.media as Media
+  const { setCategoryFilters } = useFilter()
 
   return (
     <Link
