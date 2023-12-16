a50ee3511991202dc129931256c1754297083d7a
dzekiczan1 <p.rzadkowolski@gmail.com>
2023-12-16 18:41:58 +0100

    create filters component

--- src/app/(pages)/products/page.tsx
+++ src/app/(pages)/products/page.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
 
+import { Gutter } from '../../_components/Gutter'
+import Filters from './Filters'
+
+import classes from './index.module.scss'
+
 const Product = () => {
-  return <div>Product</div>
+  return (
+    <div className={classes.container}>
+      <Gutter>
+        <Filters />
+      </Gutter>
+    </div>
+  )
 }
 
 export default Product
