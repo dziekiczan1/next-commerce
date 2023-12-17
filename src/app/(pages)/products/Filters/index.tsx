7ea253e00171a12b7144980c4f0373bccc800d4b
dzekiczan1 <p.rzadkowolski@gmail.com>
2023-12-17 10:53:35 +0100

    add filters page logic

--- src/app/(pages)/products/Filters/index.tsx
+++ src/app/(pages)/products/Filters/index.tsx
@@ -5,6 +5,7 @@ import React from 'react'
 import { Category } from '../../../../payload/payload-types'
 import { Checkbox } from '../../../_components/Checkbox'
 import { HR } from '../../../_components/HR'
+import { RadioButton } from '../../../_components/Radio'
 import { useFilter } from '../../../_providers/Filter'
 
 import classes from './index.module.scss'
@@ -42,6 +43,24 @@ const Filters = ({ categories }: { categories: Category[] }) => {
               />
             )
           })}
+        </div>
+        <HR className={classes.hr} />
+        <h6 className={classes.title}>Sort By</h6>
+        <div className={classes.categories}>
+          <RadioButton
+            label="Latest"
+            value="-createdAt"
+            isSelected={sort === '-createdAt'}
+            onRadioChange={handleSort}
+            groupName="sort"
+          />
+          <RadioButton
+            label="Oldest"
+            value="createdAt"
+            isSelected={sort === 'createdAt'}
+            onRadioChange={handleSort}
+            groupName="sort"
+          />
         </div>
       </div>
     </div>
